ee99b9402e106ec16273b1d06b35cd14464ac529
fiandinw <alifiandinursanni@gmail.com>
2023-01-03 11:35:43 +0700

    style layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import ColorCard from "./components/ColorCard";
 
 function App() {
   return (
-    <main className="w-screen h-screen bg-gradient-to-tr from-[#3F0071] to-[#FB2576] ">
-      <h1 className="capitalize font-bold text-xl text-white">
+    <main className="w-screen min-h-screen bg-gradient-to-tr from-[#3F0071] to-[#FB2576]">
+      <div className="h-16"></div>
+      <h1 className="capitalize font-bold text-2xl text-white w-fit mx-auto">
         color palette generator
       </h1>
       {/* color cards container */}
-      <div>
+      <div className="mx-auto w-fit flex flex-row gap-6 flex-wrap items-center justify-center mt-12">
+        <ColorCard />
+        <ColorCard />
+        <ColorCard />
+        <ColorCard />
         <ColorCard />
       </div>
+      <div className="h-16"></div>
     </main>
   );
 }
