98ff5edb3b6f9cb3ca9fdaefc7c61fe85889a65b
fiandinw <alifiandinursanni@gmail.com>
2023-01-03 10:56:42 +0700

    style main page and create color card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import ColorCard from "./components/ColorCard";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="w-screen h-screen bg-gradient-to-tr from-[#3F0071] to-[#FB2576] ">
+      <h1 className="capitalize font-bold text-xl text-white">
+        color palette generator
+      </h1>
+      {/* color cards container */}
+      <div>
+        <ColorCard />
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/ColorCard.jsx
@@ -0,0 +1,10 @@
+function ColorCard() {
+  return (
+    <div className="bg-white rounded-lg w-56 h-80 px-4 pt-4 flex flex-col items-center">
+      <div className="w-full h-5/6 bg-black rounded-lg"></div>
+      <p className="text-gray-600 font-bold my-auto">#COLORHEX</p>
+    </div>
+  );
+}
+
+export default ColorCard;
